87b304535dd877b6961c16c2107cfdd0e430ab9e
Abhishek Juneja <abhishek.juneja145@gmail.com>
2016-05-31 19:46:52 +0530

    quote formatting

--- js/starterPage.js
+++ js/starterPage.js
@@ -9,8 +9,8 @@
     //Changes the backgroud to a random color
     //hex codes obtained from https://www.materialui.co/colors
     var allColors = ['#212121', '#263238', '#455A64', '#5D4037', '#607D8B', '#424242', '#3E2723', '#424242', '#616161', '#34495e', '#4B0082', '#483D8B', '#008B8B', '#191970', '#696969', '#808000', '#008B8B'];
+
     var randColor = allColors[Math.floor(Math.random() * allColors.length)];
-    console.log(randColor);
 
     document.body.style.backgroundColor = randColor;
 
@@ -31,7 +31,6 @@
             var hours = Clock.zeros(date.getHours());
             var minutes = Clock.zeros(date.getMinutes());
             var time = hours + ' ' + minutes;
-            console.log(hours);
             Clock.el.innerHTML = time;
         }
     };
@@ -83,7 +82,7 @@
 
 
     // dodo mods
-    
+
     //array shuffler 
     function shuffle(array) {
         var currentIndex = array.length,
@@ -105,34 +104,58 @@
         return array;
     }
 
+
+    // Fuck callbacks
+
+
+
     // //quote parser
     // var quoteParser = function(currQuote){
-    //         var quote_arr = currQuote.split('')
+    //     var quote_arr = currQuote.split('-')
+    //     var return_quote = '';
+    //     var author = 'Anon';
+
+    //     if (quote_arr.length > 1){
+    //         author = quote_arr.pop()
+    //         console.log(author);    
+    //     }
+
+    //     var first_elem = quote_arr[0];
+    //     first_elem = '<i>"' + first_elem;
+    //     quote_arr[0] = first_elem;
+
+    //     if (quote_arr.length > 1){
+    //         var last_elem = quote_arr[-1];
+    //         last_elem = last_elem + '</i>" - ';
+    //         quote_arr[-1] = last_elem;
+    //         return_quote = quotes_arr.join(' ');
+    //     }
+    //     else{
+    //         return_quote = quote_arr.join(' ') + '"</i>" - '
+    //     }
+
+    //     return_quote += author;
+    //     console.log(return_quote);
+    //     return return_quote;
     // };
 
     //greeting message based on local time
     var greetingMessage = function() {
         var curr_date = new Date();
         var hours = curr_date.getHours();
-        console.log("Greeting Message");
-        console.log(hours);
         var curr_hour = parseInt(hours);
 
         if (12 > curr_hour && curr_hour >= 5) {
-            var morningMessages = ["Rise and Shine"];
-            console.log("Morning");
+            var morningMessages = ["Rise and Shine", "Early bird catches the.."];
             selectedPeriod = morningMessages;
         } else if (16 > curr_hour && curr_hour >= 12) {
-            var afternoonMessages = ['Time for a siesta!'];
-            console.log("Aftnoon");
+            var afternoonMessages = ['Time for a siesta!', 'Power Nap!'];
             selectedPeriod = afternoonMessages;
         } else if (22 > curr_hour && curr_hour >= 16) {
-            var eveningMessages = ['Good evening.'];
-            console.log("evening");
+            var eveningMessages = ['Good evening.', 'What\'s cookin\' for dinner?'];
             selectedPeriod = eveningMessages;
         } else if (22 >= curr_hour || (curr_hour > 0 && curr_hour <= 5)) {
-            var sleepMessages = ['You should probably go to bed...'];
-            console.log("You should sleep!")
+            var sleepMessages = ['You should probably go to bed...', 'Staying up late is not a healthy habit'];
             selectedPeriod = sleepMessages;
         }
 
@@ -147,14 +170,43 @@
 
         //read quotes from quotes.txt into an array
         var allQuotes = [];
-        var filename = 'data/quotes.txt';     
-        
-        $.get(filename, function(data){
+        var filename = 'data/quotes.txt';
+
+        $.get(filename, function(data) {
+
             allQuotes = data.split('\n');
+            var quote_start = '<i>“';
+            var quote_end = '”</i> ~ ';
+
+            for (var i = allQuotes.length - 1; i >= 0; i--) {
+
+                var quote_arr = allQuotes[i].split('-')
+                var return_quote = '';
+                var author = 'Anon';
+
+                if (quote_arr.length > 1) {
+                    author = quote_arr.pop()
+                }
+
+                var first_elem = quote_arr[0];
+                first_elem = quote_start + first_elem;
+                quote_arr[0] = first_elem;
+
+                if (quote_arr.length > 1) {
+                    var last_elem = quote_arr[-1];
+                    last_elem = last_elem + quote_end;
+                    quote_arr[-1] = last_elem;
+                    return_quote = quote_arr.join(' ');
+                } else {
+                    return_quote = quote_arr.join(' ') + quote_end;
+                }
+
+                return_quote += author;
+                allQuotes[i] = return_quote;
+            }
 
             var shuffledQuotes = shuffle(allQuotes);
-            console.log(shuffledQuotes.length);
-            
+
             $(".displayQuote").typed({
                 strings: shuffledQuotes,
                 typeSpeed: 0,
@@ -165,9 +217,9 @@
                 // character for cursor
                 cursorChar: "|",
 
-            });//typed.js
+            }); //typed.js
 
-        });//get
+        }); //get
     };
 
     greetingMessage();
